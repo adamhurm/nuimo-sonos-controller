78190cf08823ad555526229f282e6071b49c0605
Adam Hurm <adamhurm@gmail.com>
2021-07-21 16:52:08 -0400

    removed redundant sonos volume commands and added volume cap

--- sonoscontroller.py
+++ sonoscontroller.py
@@ -6,6 +6,11 @@ zone_list = list(soco.discover())
 def handle_event(event):
     if event.gesture == Gesture.ROTATION:
         for zone in zone_list:
-            zone.volume += event.value/50
-            print("Volume: " + str(zone.volume) + " [" + zone.player_name + "]")
+            if (zone.volume == 100 and event.value > 0) or (zone.volume == 0 and event.value < 0) or abs(event.value) < 1:
+                break #don't send -- volume level will stay the same, so avoid latency
+            else:
+                if zone.volume + event.value/50 > 85:
+                    break #set max vol of 85 to avoid blowing out my ears while testing
+                zone.volume += event.value/50
+                print("Volume: " + str(zone.volume) + " [" + zone.player_name + "]")
         #print("Rotated " + str(event.value) + ", Volume adjusted volume by " +str(event.value/50))
